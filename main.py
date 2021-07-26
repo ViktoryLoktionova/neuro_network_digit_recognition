9bc45a762b17c33566d027aac4a6e37c7a3d016e
ViktoryLoktionova <loktionova_viktory@mail.ru>
2021-07-26 19:19:01 +0300

    The neural network recognizes images of handwritten numbers.

---
+++ main.py
@@ -0,0 +1,73 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow import keras
+from tensorflow.keras.datasets import mnist  # база даных выборок mnist
+from tensorflow.keras.layers import Dense, Flatten
+% matplotlib
+inline
+
+# загрузка обучающей и тестовой выборок
+(x_train, y_train), (x_test, y_test) = mnist.load_data()  # загружаем данные из базы данных
+# x_train - изображения цифр обучающей выборки, y_train - вектор соотв значений цифр,
+x_train = x_train / 255  # нормализация входных данных
+x_test = x_test / 255  # x_test - изображения цифр тестовой выборки, y_test - вектор соотв значений цифр д/тест выборки(верное значение)
+
+y_train_cat = keras.utils.to_categorical(y_train, 10)  # преобразовывает выходные данные в вектор
+y_test_cat = keras.utils.to_categorical(y_test, 10)
+
+plt.figure(figsize=(10, 5))
+for i in range(25):
+    plt.subplot(5, 5, i + 1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.imshow(x_train[i], cmap=plt.cm.binary)
+
+plt.show()
+
+model = keras.Sequential([
+    Flatten(input_shape=(28, 28, 1)),
+    Dense(128, activation='relu'),
+    Dense(10, activation='softmax')
+])
+
+print(model.summary())  # вывод структуры НС в консоль
+
+# компиллируем сеть
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train_cat, batch_size=32, epochs=5, validation_split=0.2)
+model.evaluate(x_test, y_test_cat)
+
+n = 150
+x = np.expand_dims(x_test[n], axis=0)
+res = model.predict(x)
+print(res)
+print(f'Распознанная цифра: {np.argmax(res)}')
+
+plt.imshow(x_test[n], cmap=plt.cm.binary)
+plt.show()
+
+pred = model.predict(x_test)
+pred = np.argmax(pred, axis=1)
+
+print(pred.shape)
+print(pred[:20])
+print(y_test[:20])
+
+mask = pred == y_test
+print(mask[:10])
+
+x_false = x_test[~mask]
+p_false = pred[~mask]
+print(x_false.shape)
+
+plt.figure(figsize=(10, 5))
+for i in range(5):
+    plt.subplot(5, 5, i + 1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.imshow(x_false[i], cmap=plt.cm.binary)
+
+plt.show()
